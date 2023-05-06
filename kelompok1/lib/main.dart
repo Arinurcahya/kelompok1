53eaac5cc016bd6120d2c69865fafa782228ebd1
Ari Nurcahya <arinurcahyaari@gmail.com>
2023-05-06 21:11:28 +0700

    yaml

--- kelompok1/lib/main.dart
+++ kelompok1/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:kelompok1/menu.dart';
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    title: 'BIODATA ARI NURCAHYA',
+    title: 'Kelompok1',
     home: menu(),
   ));
 }
